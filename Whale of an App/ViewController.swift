bc253f2c7fbe3e96ae6c30e59636c03e3300aa37
Ramiro H Lopez <ramiro.h.lopez@outlook.com>
2021-02-25 16:36:13 -0800

    Initial Commit

---
+++ Whale of an App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Whale of an App
+//
+//  Created by Ramiro H. Lopez on 2/25/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

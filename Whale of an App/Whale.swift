d55abccb9f7bb7a5f574521ea663fb240870040a
Ramiro H Lopez <ramiro.h.lopez@outlook.com>
2021-02-26 10:47:03 -0800

    Added Whale model, WhaleData.json

---
+++ Whale of an App/Whale.swift
@@ -0,0 +1,13 @@
+//
+//  Whale.swift
+//  Whale of an App
+//
+//  Created by Ramiro H. Lopez on 2/26/21.
+//
+
+import Foundation
+
+struct Whale: Codable {
+    let name: String
+    let imagePath: String
+}

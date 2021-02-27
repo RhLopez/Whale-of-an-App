b9deb9df771a7e2af56d3e10fcb464672c6d86e6
Ramiro H Lopez <ramiro.h.lopez@outlook.com>
2021-02-26 17:45:13 -0800

    Fetch whales in WhalesCollectionControllerViewModel

--- Whale of an App/WhalesCollectionControllerViewModel.swift
+++ Whale of an App/WhalesCollectionControllerViewModel.swift
@@ -8,14 +8,17 @@
 import Foundation
 import Combine
 
-struct WhalesCollectionControllerViewModel {
+class WhalesCollectionControllerViewModel {
     let dataProvider: DataProvider
+    let whales = CurrentValueSubject<[Whale], Never>([])
     
     init(dataProvider: DataProvider) {
         self.dataProvider = dataProvider
     }
     
     func fetchWhales() {
-        
+        dataProvider.fetchWhales { [weak self] whales in
+            self?.whales.send(whales)
+        }
     }
 }

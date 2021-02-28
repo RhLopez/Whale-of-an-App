2fa17d795a7750db52374fb726f74c2d18a33b99
Ramiro H Lopez <ramiro.h.lopez@outlook.com>
2021-02-28 14:56:26 -0800

    Added loading status enum and fetch remote image function

--- Whale of an App/WhaleCollectionCellViewModel.swift
+++ Whale of an App/WhaleCollectionCellViewModel.swift
@@ -5,16 +5,51 @@
 //  Created by Ramiro H. Lopez on 2/26/21.
 //
 
-import Foundation
+import UIKit
+import Combine
 
-struct WhaleCollectionCellViewModel {
-    let whale: Whale
-    
-    init(whale: Whale) {
-        self.whale = whale
-    }
+enum LoadingStatus {
+    case loading
+    case loaded
+    case iddle
+}
+
+class WhaleCollectionCellViewModel {
+    private let whale: Whale
+    private let backgroundColor: UIColor
+    private let imageLoader: RemoteImageLoader
+    let whaleImage = CurrentValueSubject<UIImage?, Never>(nil)
+    let loadingStatus = CurrentValueSubject<LoadingStatus, Never>(.iddle)
     
     var whaleName: String {
         return whale.name
+    }
+    
+    var cellBackgroundColor: UIColor {
+        return backgroundColor
+    }
+    
+    init(whale: Whale, backgroundColor: UIColor, imageLoader: RemoteImageLoader) {
+        self.whale = whale
+        self.backgroundColor = backgroundColor
+        self.imageLoader = imageLoader
+    }
+    
+    func fetchRemoteImage() {
+        loadingStatus.send(.loading)
+        
+        imageLoader.fetchImage(withPath: whale.imagePath) { [weak self] result in
+            var image: UIImage?
+            
+            switch result {
+            case let .success(remoteImage):
+                image = remoteImage
+            case .error:
+                image = UIImage(named: "placeholder")
+            }
+            
+            self?.loadingStatus.send(.loaded)
+            self?.whaleImage.send(image)
+        }
     }
 }

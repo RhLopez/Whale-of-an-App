6e7b08d90c87c059f04420c4c324303e58a190af
Ramiro H Lopez <ramiro.h.lopez@outlook.com>
2021-02-28 14:55:14 -0800

    Updated properties access level and added method to create cell viewModel

--- Whale of an App/WhalesCollectionControllerViewModel.swift
+++ Whale of an App/WhalesCollectionControllerViewModel.swift
@@ -5,20 +5,41 @@
 //  Created by Ramiro H. Lopez on 2/26/21.
 //
 
-import Foundation
+import UIKit
 import Combine
 
 class WhalesCollectionControllerViewModel {
-    let dataProvider: DataProvider
+    private let dataProvider: DataProvider
+    private let imageLoader: RemoteImageLoader
+    private let cellBackgroundColors = [
+        UIColor.systemGreen,
+        UIColor.orange,
+        UIColor.red,
+        UIColor.purple
+    ]
     let whales = CurrentValueSubject<[Whale], Never>([])
+    let backgroundColor = UIColor.systemBackground
     
-    init(dataProvider: DataProvider) {
+    init(dataProvider: DataProvider, imageLoader: RemoteImageLoader) {
         self.dataProvider = dataProvider
+        self.imageLoader = imageLoader
     }
     
     func fetchWhales() {
         dataProvider.fetchWhales { [weak self] whales in
             self?.whales.send(whales)
         }
+    }
+    
+    func cellViewModel(at indexPath: IndexPath) -> WhaleCollectionCellViewModel {
+        let item = indexPath.item
+        let index = item < cellBackgroundColors.count ? item : item % cellBackgroundColors.count
+        let backgroundColor = cellBackgroundColors[index]
+        let whale = whales.value[indexPath.item]
+        return WhaleCollectionCellViewModel(whale: whale, backgroundColor: backgroundColor, imageLoader: imageLoader)
+    }
+    
+    func didSelectItem(at indexPath: IndexPath) {
+        
     }
 }

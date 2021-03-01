47b75b496885d808b67512aae61406ff5dad452d
Ramiro H Lopez <ramiro.h.lopez@outlook.com>
2021-02-28 17:13:42 -0800

    Added WhaleCard

---
+++ Whale of an App/WhaleCard.swift
@@ -0,0 +1,13 @@
+//
+//  Card.swift
+//  Whale of an App
+//
+//  Created by Ramiro H. Lopez on 2/28/21.
+//
+
+import UIKit
+
+struct WhaleCard: Hashable {
+    let whale: Whale
+    let backgroundColor: String
+}

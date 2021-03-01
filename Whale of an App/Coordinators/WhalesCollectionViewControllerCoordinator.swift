b2be4f381f9af4cdc60275e621a93162ea22d277
Ramiro H Lopez <ramiro.h.lopez@outlook.com>
2021-02-28 17:14:48 -0800

    Conform to WhalesCollectionControllerViewModelDelegate

--- Whale of an App/Coordinators/WhalesCollectionViewControllerCoordinator.swift
+++ Whale of an App/Coordinators/WhalesCollectionViewControllerCoordinator.swift
@@ -15,16 +15,29 @@ final class WhalesCollectionViewControllerCoordinator: Coordinator {
     
     init(rootViewController: UIViewController) {
         self.rootViewController = rootViewController
-        self.dataProvider = JSONDataProvider(fileName: "WhaleData")
+        self.dataProvider = JSONDataProvider()
         self.imageLoader = WhaleRemoteImageLoader()
     }
     
     func start() {
         let viewModel = WhalesCollectionControllerViewModel(dataProvider: dataProvider, imageLoader: imageLoader)
+        viewModel.delegate = self
         let viewController = WhalesCollectionViewController(viewModel: viewModel)
         
         rootViewController.view.addSubview(viewController.view)
         rootViewController.addChild(viewController)
         viewController.didMove(toParent: rootViewController)
+    }
+    
+    func whaleCardDetailControllerCoordinator(card: WhaleCard) {
+        let coordinator = WhaleCardDetailControllerCoordinator(card: card, rootViewController: rootViewController)
+        childCoordinators.append(coordinator)
+        coordinator.start()
+    }
+}
+
+extension WhalesCollectionViewControllerCoordinator: WhalesCollectionControllerViewModelDelegate {
+    func didSelect(card: WhaleCard) {
+        whaleCardDetailControllerCoordinator(card: card)
     }
 }
